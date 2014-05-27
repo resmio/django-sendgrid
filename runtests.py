c5420f0db334346117b10485169863d615b6dc78
Jann Kleen <jann.kleen@freshx.de>
2014-05-27 17:00:44 +0200

    initial commit

---
+++ runtests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+
+import django
+from django.conf import settings
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS=(
+            '{{ app_name }}',
+        ),
+        SITE_ID=1,
+        SECRET_KEY='this-is-just-for-tests-so-not-that-secret',
+    )
+
+
+from django.test.utils import get_runner
+
+
+def run_tests():
+    if hasattr(django, 'setup'):
+        django.setup()
+    apps = sys.argv[1:] or ['{{ app_name }}', ]
+    test_runner = get_runner(settings)
+    test_runner = test_runner(verbosity=1, interactive=True, failfast=False)
+    failures = test_runner.run_tests(apps)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    run_tests()
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='django-sendgrid',
+    version=__import__('sendgrid').__version__,
+    author='Jann Kleen',
+    author_email='jann.kleen@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/resmio/django-sendgrid',
+    license='BSD 2-Clause',
+    description=u' '.join(__import__('sendgrid').__doc__.splitlines()).strip(),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.md'),
+    test_suite='runtests.run_tests',
+    zip_safe=False,
+)
