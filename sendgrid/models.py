ee500f4986c8d50b0f73b81717d1c37024bffb65
Jann Kleen <jann.kleen@freshx.de>
2014-05-28 18:23:48 +0200

    add generic FK and verbose names

--- sendgrid/models.py
+++ sendgrid/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext as _
+
 
 import uuid
 
 
-class EmailEvent(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+class Email(models.Model):
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
 
-    email = models.CharField(max_length=512)
-    event = models.CharField(max_length=32)
-    timestamp = models.DateTimeField()
-    uuid = models.CharField(max_length=64, default=lambda: str(uuid.uuid4()))
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    email = models.CharField(_('addressee'), max_length=512)
+    event = models.CharField(_('event type'), max_length=32)
+    timestamp = models.DateTimeField(_('timestamp'))
+    uuid = models.CharField(_('reference UUID'), max_length=64, default=lambda: str(uuid.uuid4()))
 
     def __unicode__(self):
         return '%s: %s' % (self.email, self.event)
