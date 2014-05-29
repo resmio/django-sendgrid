23c445992caff6d55f60502b62e1ad87da00d322
Jann Kleen <jann.kleen@freshx.de>
2014-05-29 13:29:09 +0200

    lazy translations to have a working django 1.7 testing

--- sendgrid/models.py
+++ sendgrid/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 import uuid
