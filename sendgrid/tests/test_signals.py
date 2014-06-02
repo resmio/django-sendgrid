94bfbaa273a9d064ac0bd175109d073b39f3b1ef
Jann Kleen <jann.kleen@freshx.de>
2014-06-02 17:28:23 +0200

    make signals test actually do something

--- sendgrid/tests/test_signals.py
+++ sendgrid/tests/test_signals.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 
-from sendgrid import utils
+from sendgrid import utils, signals
 
 import json
 
@@ -16,8 +16,20 @@ class SignalTestCase(TestCase):
     def test_received_email(self):
         """ Test signals triggered by sendgrid callback.
         """
+        data = []
+
+        def email_event_handler(sender, signal):
+            data.append((sender, signal, ))
+
+        signals.email_event.connect(email_event_handler)
+
+        # check if we received signals
+        self.assertEqual(len(data), 0)
         message = utils.SendgridEmailMessage(**self.email_data)
         message.send()
+
+        # check if we received signals
+        self.assertEqual(len(data), 0)
 
         response = self.client.post('/sendgrid_callback/',
                                     data=json.dumps([{
@@ -27,4 +39,10 @@ class SignalTestCase(TestCase):
                                         'timestamp': '123456789',
                                     }, ]),
                                     content_type='application/json')
+
+        # verify that we received a signal
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0][0].event, 'processed')
+        self.assertEqual(data[0][0].uuid, message.uuid)
+
         self.assertEqual(response.status_code, 200)
