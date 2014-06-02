b2e25cc5f1bf5507deccc7c9a3ba69c261a6a9a2
Jann Kleen <jann.kleen@freshx.de>
2014-06-02 16:50:24 +0200

    add signals

---
+++ sendgrid/signals.py
@@ -0,0 +1,3 @@
+import django.dispatch
+
+email_event = django.dispatch.Signal()
---
+++ sendgrid/tests/test_signals.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, Client
+
+from sendgrid import signals
+from sendgrid import utils
+
+import json
+
+
+class SignalTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.email_data = {'subject': 'Test Subject',
+                           'body': 'Hi, I am a test body',
+                           'from_email': 'email@example.com',
+                           'to': ('other_email@example.com', )}
+
+    def test_received_email(self):
+        """ Test signals triggered by sendgrid callback.
+        """
+        message = utils.SendgridEmailMessage(**self.email_data)
+        message.send()
+
+        response = self.client.post('/sendgrid_callback/',
+                                    data=json.dumps([{
+                                        'email': 'other_email@example.com',
+                                        'uuid': message.uuid,
+                                        'event': 'processed',
+                                        'timestamp': '123456789',
+                                    }, ]),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
