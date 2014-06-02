139f366a16a26ee71ff9b9314680d51b49d736fe
Jann Kleen <jann.kleen@freshx.de>
2014-06-02 15:34:39 +0200

    DRYed tests

--- sendgrid/tests/test_utils.py
+++ sendgrid/tests/test_utils.py
@@ -8,51 +8,45 @@ import json
 
 
 class UtilTestCase(TestCase):
+    def setUp(self):
+        self.email_data = {'subject': 'Test Subject',
+                           'body': 'Hi, I am a test body',
+                           'from_email': 'email@example.com',
+                           'to': ('other_email@example.com', )}
+
     def test_django_email(self):
-        message = EmailMessage(subject='Test Subject',
-                               body='Hi, I am a test body',
-                               from_email='email@example.com',
-                               to=('other_email@example.com', ))
+        message = EmailMessage(**self.email_data)
         message.send()
         # this email should not have a UUID
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Test Subject')
+        self.assertEqual(mail.outbox[0].subject, self.email_data['subject'])
         self.assertNotIn('X-SMTPAPI', mail.outbox[0].extra_headers)
 
     def test_django_multipart(self):
-        message = EmailMultiAlternatives(subject='Test Subject',
-                                         body='Hi, I am a test body',
-                                         from_email='email@example.com',
-                                         to=('other_email@example.com', ))
+        message = EmailMultiAlternatives(**self.email_data)
         message.send()
         # this email should not have a UUID
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Test Subject')
+        self.assertEqual(mail.outbox[0].subject, self.email_data['subject'])
         self.assertNotIn('X-SMTPAPI', mail.outbox[0].extra_headers)
 
     def test_sendgrid_email(self):
-        message = SendgridEmailMessage(subject='Test Subject',
-                                       body='Hi, I am a test body',
-                                       from_email='email@example.com',
-                                       to=('other_email@example.com', ))
+        message = SendgridEmailMessage(**self.email_data)
         message.send()
         # this email should have a UUID
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Test Subject')
+        self.assertEqual(mail.outbox[0].subject, self.email_data['subject'])
         self.assertIn('X-SMTPAPI', mail.outbox[0].extra_headers)
         content = json.loads(mail.outbox[0].extra_headers['X-SMTPAPI'])
         self.assertIn('uuid', content['unique_args'])
         self.assertEqual(len(content['unique_args']['uuid']), 36)
 
     def test_sendgrid_multipart(self):
-        message = SendgridEmailMultiAlternatives(subject='Test Subject',
-                                                 body='Hi, I am a test body',
-                                                 from_email='email@example.com',
-                                                 to=('other_email@example.com', ))
+        message = SendgridEmailMultiAlternatives(**self.email_data)
         message.send()
         # this email should have a UUID
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Test Subject')
+        self.assertEqual(mail.outbox[0].subject, self.email_data['subject'])
         self.assertIn('X-SMTPAPI', mail.outbox[0].extra_headers)
         content = json.loads(mail.outbox[0].extra_headers['X-SMTPAPI'])
         self.assertIn('uuid', content['unique_args'])
