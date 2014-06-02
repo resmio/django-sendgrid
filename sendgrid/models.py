e90d854550a49976bcb93c0f6ffe1b689081e684
Jann Kleen <jann.kleen@freshx.de>
2014-06-02 12:29:25 +0200

    formatting

--- sendgrid/models.py
+++ sendgrid/models.py
@@ -3,7 +3,6 @@ from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 
-
 import uuid
 
 
