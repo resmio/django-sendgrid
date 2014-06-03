eab1de115f010922531a5a2c5f023bf2294f2af4
Jann Kleen <jann.kleen@freshx.de>
2014-06-03 16:16:45 +0200

    Revert "add __all__ parameter to main module"
    This reverts commit bc9e574206e75b1a50bd1b8eb4bd56f96a18cf51.

--- sendgrid/__init__.py
+++ sendgrid/__init__.py
@@ -1,8 +1,3 @@
 """A small django app around sendgrid and its webhooks"""
 
-from utils import SendgridEmailMessage, SendgridEmailMultiAlternatives
-from models import Email
-from signals import email_event
-
 __version__ = '0.1.0'
-__all__ = ('SendgridEmailMessage', 'SendgridEmailMultiAlternatives', 'Email', 'email_event')
