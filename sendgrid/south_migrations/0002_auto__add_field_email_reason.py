6ea43a0e49389b2fad10a8120d187c246ffe88e6
Jann Kleen <jann.kleen@freshx.de>
2014-10-07 17:30:19 +0300

    pep8 fix for south migrations

--- sendgrid/south_migrations/0002_auto__add_field_email_reason.py
+++ sendgrid/south_migrations/0002_auto__add_field_email_reason.py
@@ -13,15 +13,14 @@ class Migration(SchemaMigration):
                       self.gf('django.db.models.fields.CharField')(default='', max_length=1024),
                       keep_default=False)
 
-
     def backwards(self, orm):
         # Deleting field 'Email.reason'
         db.delete_column(u'sendgrid_email', 'reason')
 
-
     models = {
         u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)",
+                     'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -29,7 +28,8 @@ class Migration(SchemaMigration):
         },
         u'sendgrid.email': {
             'Meta': {'object_name': 'Email', 'index_together': "[['content_type', 'object_id']]"},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']", 'null': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [],
+                             {'to': u"orm['contenttypes.ContentType']", 'null': 'True'}),
             'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'email': ('django.db.models.fields.CharField', [], {'max_length': '512'}),
             'event': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
@@ -38,7 +38,8 @@ class Migration(SchemaMigration):
             'reason': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1024'}),
             'timestamp': ('django.db.models.fields.DateTimeField', [], {}),
             'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'uuid': ('django.db.models.fields.CharField', [], {'default': "'6178f3a8-3602-4d90-be2d-66029be8fb42'", 'max_length': '64', 'db_index': 'True'})
+            'uuid': ('django.db.models.fields.CharField', [], {'default': "'6178f3a8-3602-4d90-be2d-66029be8fb42'",
+                                                               'max_length': '64', 'db_index': 'True'})
         }
     }
 
